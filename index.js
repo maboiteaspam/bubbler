8071170474a965833b48854bc26add4cacc3b012
clement <maboiteaspam@wanadoo.fr>
2015-12-05 21:04:18 +0800

    init

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-var debug = require('debug')
 var flower = require('flower')
+var debug = require('debug')('bubbler')
+var k = 0;
 
-var module = require('bubbler');          // your code here,
-                                          // your doc here.
+module.exports = function bubbler (bubbleizedStream, events, name, fnT, fnF) {
+  var stream;
+
+  if(!bubbleizedStream.pipe) {
+    fnF = fnT
+    fnT = name
+    name = events || 'bubbler-' + k;
+    events = bubbleizedStream
+    stream = flower(fnT, fnF);
+  } else {
+    name = name || 'bubbler-' + k;
+    stream = bubbleizedStream
+    // fnT, fnF are ignored
+  }
+
+  var index = events.indexOf('data');
+  index>-1 && events.splice(index, 1);
+
+  var eventsFn = {};
+  events.forEach(function (event) {
+    eventsFn[event] = function(message) {
+      debug('%s: bubble up message %s', name, event)
+      stream.emit(event, message)
+    };
+  })
+
+
+  var oldPipe = stream.pipe;
+  var oldUnpipe = stream.unpipe;
+
+  stream.pipe = function (s, o) {
+    events.forEach(function (event) {
+      s.on(event, eventsFn[event])
+    });
+    s.on('end', function (){
+      events.forEach(function (event) {
+        s.removeEventListener(event, eventsFn[event])
+      })
+    });
+    return oldPipe.apply(stream, [].slice.apply(arguments));
+  };
+
+  stream.unpipe = function (s, o) {
+    events.forEach(function (event) {
+      s.removeEventListener(event, eventsFn[event])
+    });
+    return oldUnpipe.apply(stream, [].slice.apply(arguments));
+  };
+
+  k++;
+
+  return stream;
+};
--- playground.js
+++ playground.js
@@ -1,2 +1,24 @@
-var module = require('bubbler');          // your code here,
-                                          // your doc here.
+
+
+var flower = require('flower')
+
+var bubbler = require('./index.js');            // bubbler gives streams ability to bubble up events.
+                                                //
+var events = ['message'];                       // It needs to know which events to bubble.
+                                                //
+var streamA = bubbler(events, 'streamA');       // bubbler signature is
+var streamB = bubbler(events, 'streamB');       //  - bubbler(events[, name[, fnTransform[, fnFlush]]]) => new stream
+var streamC = bubbler(flower(), events);        //  - bubbler(stream, events[, name]) => stream
+                                                //
+streamA.pipe(streamB);                          // When bubbled streams
+streamA.pipe(streamC);                          // are piped together.
+                                                //
+streamB.emit('message', 'hello, its streamB !') // A down stream can emit events
+streamC.emit('message', 'hello, its streamC !') // upward in the stream.
+                                                //
+streamA.on('message', function (chunk) {        // Those streams enable you to listen
+  console.log(chunk)                            // all down streams events in one handler.
+})                                              //
+                                                //
+streamB.emit('message', 'hello, its streamB !') // Note : re-trigger the events to invoke the listener,
+streamC.emit('message', 'hello, its streamC !') // so something shows up at runtime ;)
